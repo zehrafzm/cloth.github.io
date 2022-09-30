d79a9930e9c24f8834d2a63d559901a507e4f932
zehrafzm <you@example.com>
2022-09-30 18:28:46 +0300

    clone

--- src/components/ShopBag.jsx
+++ src/components/ShopBag.jsx
@@ -1,10 +1,32 @@
+import Box from '@mui/material/Box';
+import { useEffect } from 'react';
 
 export default function ShopBag(e){
     
+  const product= localStorage.getItem('productUrl')
+  const price = localStorage.getItem('totalPrice')
+  useEffect(()=>{
+    var list = document.getElementById('shopBagDiv')
     
+    list += "<img lassName='bagProductImage' src={product} />" + "<p>"+ "değişti"
+    
+  }
+  
+  )
     return(
-        <div>
+        <div id='shopBagDiv'>
           <p>shopbag</p>
+          <Box className='bagProduct' >
+            <img className='bagProductImage' src={product} /> 
+            
+          </Box>
+          <Box className='bagProduct' >
+            <img className='bagProductImage' src={product} /> 
+          </Box>
+          <Box className='bagProduct' >
+          <img className='bagProductImage' src={product} /> 
+          </Box>
+          total price: {price}
         </div>
     )
 
